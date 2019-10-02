a7e2325c6734f0c1d66e29fc22a496b59695c1f7
Rapidash99 <slavav99@mail.ru>
2019-10-02 23:22:32 +0300

    na

--- send_file.py
+++ send_file.py
@@ -1,23 +1,35 @@
-from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
-from threading import Thread
+from socket import socket, AF_INET, SOCK_STREAM
+from os.path import getsize
 import sys
-
-
-class Client(Thread):
-    def __init__(self):
-        super().__init__(daemon=True)
-
-    def run(self):
 
 
 if __name__ == '__main__':
     if sys.argv[0] == 1:
         exit("You did not specified the file, address and port correctly")
 
-    file = sys.argv[1]
-    address = sys.argv[2]
+    file_name = str(sys.argv[1])
+    address = str(sys.argv[2])
     port = int(sys.argv[3])
 
     sock = socket(AF_INET, SOCK_STREAM)
-    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     sock.connect((address, port))
+
+    sock.send(file_name.encode())
+
+    size = getsize(file_name)
+    file = open(file_name, "rb")
+    byte = file.read(1024)
+    bytes_sent = 0
+
+    print(sock.recv(1024).decode())
+
+    while byte:
+        print("Transported " + str(bytes_sent * 100 // size) + "%")
+        sock.send(byte)
+        byte = file.read(1024)
+        bytes_sent += 1024
+
+    file.close()
+    print("File transported")
+    sock.close()
+    print("Connection closed")
---
+++ server.py
@@ -0,0 +1,70 @@
+import socket
+from threading import Thread
+import os.path
+
+clients = []
+
+
+class ClientListener(Thread):
+    def __init__(self, name: str, sock: socket.socket):
+        super().__init__(daemon=True)
+        self.sock = sock
+        self.name = name
+
+    def run(self):
+        file_name = self.sock.recv(1024).decode()
+        if os.path.isfile(file_name):
+            i = 0
+            while True:
+                index = file_name.rindex('.')
+                if os.path.isfile(file_name[:index] + '(Copy_' + str(i) + ')' + file_name[index:]):
+                    i = i + 1
+                else:
+                    file_name = file_name[:index] + '(Copy_' + str(i) + ')' + file_name[index:]
+                    break
+        else:
+            sock.send("There are no file with given name".encode())
+            sock.close()
+            exit("There are no file with given name")
+
+        file = open(file_name, 'wb')
+        message = '{file_name} created'
+        self.sock.send(message.encode())
+
+        byte = self.sock.recv(1024)
+        while byte:
+            file.write(byte)
+            byte = self.sock.recv(1024)
+        self._close()
+
+    def _broadcast(self, data):
+        data = (self.name + '> ').encode() + data
+        for u in clients:
+            if u == self.sock:
+                continue
+            u.sendall(data)
+
+    def _clear_echo(self, data):
+        self.sock.sendall('\033[F\033[K'.encode())
+        data = 'me> '.encode() + data
+        self.sock.sendall(data)
+
+    def _close(self):
+        clients.remove(self.sock)
+        self.sock.close()
+        print(self.name + ' disconnected')
+
+
+if __name__ == "__main__":
+    next_name = 1
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', 8800))
+    sock.listen(10)
+    while True:
+        con, addr = sock.accept()
+        clients.append(con)
+        name = 'u' + str(next_name)
+        next_name = next_name + 1
+        print(str(addr) + ' connected as ' + name)
+        ClientListener(name, con).start()
